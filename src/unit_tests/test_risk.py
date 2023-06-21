fdd77933c0109618e59accd9d451ec7f1bee65bd
CarlosNasayo <carlosnasayov@gmail.com>
2023-06-21 13:59:31 -0500

    update unit test for risk

--- src/unit_tests/test_risk.py
+++ src/unit_tests/test_risk.py
@@ -13,7 +13,7 @@ class TestAgroadisory(unittest.TestCase):
     def test_single_risk(self):
         #this endpoint receives one paremeter after risk/(risk)) that correspond to the admistrative level 4 (Kebele)
         #examples: 63850dba9d578e523c7eabfd, 63850dba9d578e523c7eac02, 63850dfd9d578e523c7ec346
-        response = self.app.get('http://127.0.0.1:5000/risk/63850dba9d578e523c7eac02',headers={"Content-Type": "application/json"}) #statuscode 200
+        response = self.app.get('http://127.0.0.1:5000/risk/63850dba9d578e523c7eabfd/63865d9f68c981103580abef',headers={"Content-Type": "application/json"}) #statuscode 200
         #responseValidationError = self.app.get('http://127.0.0.1:5000/risk/63850d9a9d578e523c7e9bcb8',headers={"Content-Type": "application/json"}) #statusCode 500
         #responseNullArray = self.app.get('http://127.0.0.1:5000/risk/63850dfd9d578e523c7ec312',headers={"Content-Type": "application/json"}) #response a null array
         responseNotFound = self.app.get('http://127.0.0.1:5000/riskk/63850dfd9d578e523c7ec312',headers={"Content-Type": "application/json"}) #response a null array
